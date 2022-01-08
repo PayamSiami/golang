70c4915a060b1b44a933a12161c2c23c17e619e6
Payam Siami <payamsiami90@gmail.com>
2022-01-08 21:23:41 +0330

    fix app

--- src/auto/data.go
+++ src/auto/data.go
@@ -3,7 +3,7 @@ package auto
 import "api/models"
 
 var users = []models.User{
-	models.User{Nickname: "john", Email: "john", Password: "123456789"},
+	models.User{Nickname: "john", Email: "johndoe@email.com", Password: "123456789"},
 }
 
 var posts = []models.Post{
