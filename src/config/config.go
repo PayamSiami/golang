e1f76d71fdc304f9d6243a5242b0dad272e41221
Payam Siami <payamsiami90@gmail.com>
2022-01-08 21:24:08 +0330

    fix app

--- src/config/config.go
+++ src/config/config.go
@@ -2,28 +2,35 @@ package config
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"strconv"
+
+	"github.com/joho/godotenv"
 )
 
 var (
-	PORT     = 0
-	DBURL    = ""
-	DBDRIVER = ""
+	PORT      = 0
+	SECRETKEY []byte
+	DBURL     = ""
+	DBDRIVER  = ""
 )
 
 func Load() {
 	var err error
-	// err = godotenv.Load()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	PORT, err = strconv.Atoi(os.Getenv("API_PORT"))
 	if err != nil {
 		PORT = 9000
 	}
 
-	DBDRIVER = "mysql"
-	DBURL = fmt.Sprintf("%s:%s@/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		"foo", "p@ssW0rd", "blogos")
+	DBDRIVER = os.Getenv("DB_DRIVER")
+	DBURL = fmt.Sprintf("%s:%s@/%s?charset=utf8mb4&parseTime=True&loc=Local", os.Getenv("DB_USER"),
+		os.Getenv("DB_PASS"), os.Getenv("DB_NAME"))
+
+	SECRETKEY = []byte(os.Getenv("API_SECRET"))
 }
